117c427f7feb45ccf660c62844d1935fd588cf97
Flavio <senabrasousa@gmail.com>
2026-02-23 18:10:17 +0100

    stablev3

--- nfl-team-service/src/main/java/com/client/nflteamservice/team/TeamController.java
+++ nfl-team-service/src/main/java/com/client/nflteamservice/team/TeamController.java
@@ -1,5 +1,10 @@
 package com.client.nflteamservice.team;
 
+import com.client.nflteamservice.kafka.TeamEventProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -7,6 +12,9 @@ import reactor.core.publisher.Flux;
 @RestController
 @RequestMapping("/teams")
 public class TeamController {
+
+    @Autowired
+    TeamEventProducer teamEventProducer;
 
     @GetMapping
     public Flux<Team> getTeams() {
@@ -18,14 +26,18 @@ public class TeamController {
     }
 
     @PostMapping("/new")
-    public ResponseEntity<String> createTeam(@RequestBody Team team) {
+    public ResponseEntity<String> createTeam(@RequestBody Team team) throws JsonProcessingException {
         // Save team to database (if you have one)
         //teamService.save(team);
 
+        try {
         // Publish Kafka event
-        //String teamJson = new ObjectMapper().writeValueAsString(team);
-        //teamEventProducer.publishTeamCreated(teamJson);
+        String teamJson = new ObjectMapper().writeValueAsString(team);
+        teamEventProducer.publishTeamCreated(teamJson);
+        } catch(Exception e) {
+            return ResponseEntity.ok(e.getMessage());
+        }
 
-        return ResponseEntity.ok("Team created and event published");
+        return ResponseEntity.ok("Team created and event published v2");
     }
 }
