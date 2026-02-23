2c7aadb56e856e6a54989ba165c3e97a2eeae725
Flavio <senabrasousa@gmail.com>
2026-02-23 15:28:49 +0100

    error

--- nfl-team-service/src/main/java/com/client/nflteamservice/NflTeamServiceApplication.java
+++ nfl-team-service/src/main/java/com/client/nflteamservice/NflTeamServiceApplication.java
@@ -1,4 +1,4 @@
-package com.client.nfl_team_service;
+package com.client.nflteamservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
