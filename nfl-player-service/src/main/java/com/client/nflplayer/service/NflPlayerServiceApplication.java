b39cf62fcb8617e05336f5023e746a587e0fdacd
Flavio <senabrasousa@gmail.com>
2026-03-28 12:48:44 +0100

    new ms

---
+++ nfl-player-service/src/main/java/com/client/nflplayer/service/NflPlayerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.client.nflplayer.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NflPlayerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NflPlayerServiceApplication.class, args);
+    }
+
+}
